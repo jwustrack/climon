f446aed0610be7edf540ae9f638792053fe7566d
Jonas Wustrack <jonas.wustrack@laposte.net>
2017-12-10 20:56:59 +0100

    Lot's of cleanup.

--- database.py
+++ database.py
@@ -14,7 +14,7 @@ from math import floor
 
 # Intervals between values in seconds.
 # The target is to have graphs show approx 100 values.
-view_ranges = dict(
+VIEW_RANGES = dict(
     hour=td(minutes=1),
     day=td(minutes=10),
     week=td(hours=1),
@@ -33,96 +33,125 @@ def round_datetime(dt, view_range):
     >>> round_datetime(dt, 'week')
     datetime.datetime(2017, 8, 28, 14, 0)
     >>> round_datetime(dt, 'month')
-    datetime.datetime(2017, 8, 28, 12, 0)
-    >>> round_datetime(dt, 'year')
     datetime.datetime(2017, 8, 28, 0, 0)
+    >>> round_datetime(dt, 'year')
+    datetime.datetime(2017, 8, 24, 0, 0)
     '''
-    interval = view_ranges[view_range].total_seconds()
-    ts = int(dt.replace(tzinfo=tz.utc).timestamp() / interval) * interval
-    return datetime.datetime.utcfromtimestamp(ts)
+    interval = VIEW_RANGES[view_range].total_seconds()
+    timestamp = int(dt.replace(tzinfo=tz.utc).timestamp() / interval) * interval
+    return datetime.datetime.utcfromtimestamp(timestamp)
 
 def iter_view_times(time_from, time_to, view_range):
     '''
-    >>> start, end = datetime.datetime(2017, 8, 28, 14), datetime.datetime(2017, 9, 3, 00, 51)
-    >>> list(iter_view_times(start, end, 'year'))
-    [datetime.datetime(2017, 8, 31, 0, 0), datetime.datetime(2017, 9, 3, 0, 0)]
-    >>> list(iter_view_times(datetime.datetime(2017, 8, 28, 14, 15, 31), datetime.datetime(2017, 8, 28, 14, 21, 31), 'day'))
+    >>> d1 = datetime.datetime(2017, 8, 28, 14, 15, 31)
+    >>> d2 = datetime.datetime(2017, 9, 7,  0, 51)
+    >>> d3 = datetime.datetime(2017, 8, 28, 14, 21, 31)
+    >>> list(iter_view_times(d1, d2, 'year'))
+    [datetime.datetime(2017, 8, 31, 0, 0), datetime.datetime(2017, 9, 7, 0, 0)]
+    >>> list(iter_view_times(d1, d3, 'day'))
     [datetime.datetime(2017, 8, 28, 14, 20)]
     '''
     current = round_datetime(time_from, view_range)
-    while (current < time_to):
+    while current < time_to:
         if current >= time_from:
             yield current
-        current += view_ranges[view_range]
+        current += VIEW_RANGES[view_range]
+
+def null_stats(view_times):
+    '''
+    >>> null_stats(range(4)) # doctest: +NORMALIZE_WHITESPACE
+    [(0, None, None, None, None, None, None),
+     (1, None, None, None, None, None, None),
+     (2, None, None, None, None, None, None),
+     (3, None, None, None, None, None, None)]
+    '''
+    res = []
+    for view_time in view_times:
+        res.append((view_time, None, None, None, None, None, None))
+    return res
+
+def get_view_times(rows):
+    view_times = set()
+    for row in rows:
+        time, _, _, _, _, _, _ = row
+        view_times.add(time)
+    return view_times
+
+def pack_by(l, n):
+    '''
+    >>> list(pack_by(list(range(10)), 3))
+    [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9]]
+    '''
+    rest = l
+    while rest:
+        curr, rest = rest[:n], rest[n:]
+        yield curr
 
 class DB(object):
+    'Base Database class'
 
     def __init__(self, fname):
-        self.db = sqlite3.connect(fname, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+        self.db = sqlite3.connect(fname,
+                                  detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
 
     def close(self):
         self.db.close()
 
 class ReadDB(DB):
+    'Read-only Database class.'
 
     def get(self, sensor, time_from, time_to):
-        logging.debug('Getting data for sensor %r from %r to %r' % (sensor, time_from, time_to))
-        cursor = self.db.execute('SELECT time, temperature, humidity FROM climon WHERE sensor = ? AND time >= ? AND time < ? ORDER BY time ASC', (sensor, time_from, time_to))
+        logging.debug('Getting data for sensor %r from %r to %r', sensor, time_from, time_to)
+        cursor = self.db.execute('\
+                SELECT time, temperature, humidity\
+                FROM climon\
+                WHERE sensor = ? AND time >= ? AND time < ?\
+                ORDER BY time ASC', (sensor, time_from, time_to))
 
         for row in cursor.fetchall():
             yield row
 
-    def get_null_stats(self, view_times):
-        for view_time in view_times:
-            yield view_time, None, None, None, None, None, None
-
-    def get_view_times(self, rows):
-        view_times = set()
-        for row in rows:
-            time, _, _, _, _, _, _ = row
-            view_times.add(time)
-        return view_times
-
     def get_stats(self, sensor, time_from, time_to, view_range):
-        assert view_range in view_ranges
+        assert view_range in VIEW_RANGES
 
         view_times = set(iter_view_times(time_from, time_to, view_range))
 
         # Don't attempt to get anything outside of the date range
-        minDate, maxDate = self.getDateSpan()
+        db_time_from, db_time_to = self.get_date_span()
+        time_from = max(db_time_from, time_from)
+        time_to = min(db_time_to, time_to)
 
-        null_view_times = [vtime for vtime in view_times if vtime < minDate or vtime > maxDate]
-        logging.debug('Null stats: %r' % null_view_times)
+        cursor = self.db.execute('\
+                SELECT time [timestamp],\
+                    temperature_avg, temperature_min, temperature_max,\
+                    humidity_avg, humidity_min, humidity_max\
+                FROM climon_stats\
+                WHERE sensor = ? AND view_range = ? AND time >= ? AND time < ?\
+                ORDER BY time ASC', (sensor, view_range, time_from, time_to))
+        rows = cursor.fetchall()
+
+        logging.debug('Found %d rows in stats table', len(rows))
 
         # Fill anything outside of what we have in DB with NULL values
-        rows = list(self.get_null_stats([vtime for vtime in view_times if vtime < minDate or vtime > maxDate]))
-    
-        time_from = max(minDate, time_from)
-        time_to = min(maxDate, time_to)
-
-        cursor = self.db.execute('SELECT time [timestamp], temperature_avg, temperature_min, temperature_max, humidity_avg, humidity_min, humidity_max FROM climon_stats WHERE sensor = ? AND view_range = ? AND time >= ? AND time < ? ORDER BY time ASC', (sensor, view_range, time_from, time_to))
-
-        stat_view_times = set()
-        rows += cursor.fetchall()
-        logging.debug('Found %d rows in stats table' % len(rows))
-
-        missing_view_times = view_times - self.get_view_times(rows)
-        if missing_view_times:
-            rows += list(self.get_null_stats(missing_view_times))
+        rows += null_stats(view_times - get_view_times(rows))
 
         return sorted(rows, key=lambda r: r[0])
 
-    def getLatest(self, sensor):
-        cursor = self.db.execute('SELECT time, temperature, humidity FROM climon WHERE sensor = ? ORDER BY time desc LIMIT 1', (sensor,))
+    def get_latest(self, sensor):
+        cursor = self.db.execute('SELECT time, temperature, humidity\
+                FROM climon\
+                WHERE sensor = ?\
+                ORDER BY time desc\
+                LIMIT 1', (sensor,))
         return cursor.fetchone()
 
-    def getDateSpan(self):
+    def get_date_span(self):
         # Getting min and max separately is much faster in sqlite3
         cursor = self.db.execute('SELECT min(time) as "min_t [timestamp]" FROM climon')
-        minTime = cursor.fetchone()[0]
+        time_from = cursor.fetchone()[0]
         cursor = self.db.execute('SELECT max(time) as "max_t [timestamp]" FROM climon')
-        maxTime = cursor.fetchone()[0]
-        return minTime, maxTime
+        time_to = cursor.fetchone()[0]
+        return time_from, time_to
 
 class WriteDB(DB):
 
@@ -135,61 +164,80 @@ class WriteDB(DB):
 
     def setup(self):
         try:
-            self.db.execute("CREATE TABLE climon_stats (time timestamp, sensor, view_range, temperature_avg, temperature_min, temperature_max, humidity_avg, humidity_min, humidity_max)")
-            self.db.execute("CREATE TABLE climon (time timestamp, sensor, temperature, humidity)")
-            self.db.execute("CREATE INDEX time_index ON climon(time)")
+            self.db.execute("\
+                    CREATE TABLE climon_stats (\
+                        time timestamp, sensor, view_range,\
+                        temperature_avg, temperature_min, temperature_max,\
+                        humidity_avg, humidity_min, humidity_max,\
+                        CONSTRAINT pk PRIMARY KEY (time, sensor, view_range))")
+            self.db.execute("CREATE TABLE climon (time timestamp, sensor,\
+                        temperature, humidity,\
+                        CONSTRAINT pk PRIMARY KEY (time, sensor))")
+            self.db.execute("CREATE INDEX climon_index ON climon(sensor, time)")
             self.commit()
         except sqlite3.OperationalError:
             pass
 
-    def reindex(self):
-        for row in self.db.execute('SELECT distinct sensor FROM climon'):
-            sensor, = row
-            logging.debug('Getting timestamps for %s' % (sensor,))
-            timestamps = [row[0] for row in self.db.execute('SELECT time [timestamp] FROM climon WHERE sensor = ? ORDER BY time', (sensor,))]
-            for view_range in view_ranges:
-                self.update_view_stats(sensor, view_range, timestamps)
-        self.commit()
-
     def set(self, sensor, timestamp, temperature, humidity):
-        self.db.execute("INSERT INTO climon VALUES (?, ?, ?, ?)", (timestamp, sensor, temperature, humidity))
+        self.db.execute("INSERT INTO climon VALUES (?, ?, ?, ?)",
+                        (timestamp, sensor, temperature, humidity))
         self.update_stats(sensor, timestamp)
         self.commit()
 
     def update_view_stats(self, sensor, view_range, timestamps):
         view_timestamps = set(round_datetime(timestamp, view_range) for timestamp in timestamps)
-        logging.info('Updating stats %s %s %d' % (sensor, view_range, len(view_timestamps)))
+        logging.info('Updating stats %s %s %d', sensor, view_range, len(view_timestamps))
         stats = list(self.get_stats_from_raw(sensor, view_range, view_timestamps))
         self.set_stats(stats, sensor, view_range)
 
     def update_stats(self, sensor, timestamp):
-        for view_range in view_ranges:
+        for view_range in VIEW_RANGES:
             view_timestamp = round_datetime(timestamp, view_range)
             stats = list(self.get_stats_from_raw(sensor, view_range, [view_timestamp]))
             self.set_stats(stats, sensor, view_range)
 
     def get_stats_from_raw(self, sensor, view_range, view_times):
-        logging.debug('Getting raw stats for %r' % view_times)
-        interval = floor(view_ranges[view_range].total_seconds())
+        logging.debug('Getting raw stats for %r', view_times)
+        interval = floor(VIEW_RANGES[view_range].total_seconds())
         # we need to make datetime timezone unaware for the WHERE ... IN to work
-        view_times = [ t.replace(tzinfo=None) for t in view_times ]
+        view_times = [t.replace(tzinfo=None) for t in view_times]
         for view_times in pack_by(view_times, 9999):
-            query = 'select datetime((strftime(\'%%s\', time) / ?) * ?, \'unixepoch\') as "interval [timestamp]", avg(temperature), min(temperature), max(temperature), avg(humidity), min(humidity), max(humidity) from climon where sensor = ? AND "interval [timestamp]" in (%s) group by "interval [timestamp]" order by "interval [timestamp]"' % ",".join(["?"]*len(view_times))
-            logging.debug('Query %r args %r' % (query, [interval, interval, sensor] + view_times))
+            query = '\
+                    SELECT datetime((strftime(\'%%s\', time) / ?) * ?, \'unixepoch\') as "interval [timestamp]",\
+                        avg(temperature), min(temperature), max(temperature),\
+                        avg(humidity), min(humidity), max(humidity)\
+                    FROM climon\
+                    WHERE sensor = ? AND "interval [timestamp]" in (%s)\
+                    GROUP BY "interval [timestamp]"\
+                    ORDER BY "interval [timestamp]"' % ",".join(["?"]*len(view_times))
+            logging.debug('Query %r args %r', query, [interval, interval, sensor] + view_times)
             cursor = self.db.execute(query, [interval, interval, sensor] + view_times)
             for row in cursor:
                 yield row
 
     def set_stats(self, rows, sensor, view_range):
         for rows in pack_by(rows, 9999):
-            logging.debug('INSERT %r %r %r' % (sensor, view_range, rows))
-            self.db.executemany('INSERT INTO climon_stats (sensor, view_range, time, temperature_avg, temperature_min, temperature_max, humidity_avg, humidity_min, humidity_max) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [[sensor, view_range] + list(r) for r in rows])
+            logging.debug('INSERT %r %r %r', sensor, view_range, rows)
+            self.db.executemany('\
+                    INSERT OR REPLACE INTO climon_stats (sensor, view_range, time,\
+                        temperature_avg, temperature_min, temperature_max,\
+                        humidity_avg, humidity_min, humidity_max)\
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                                [[sensor, view_range] + list(r) for r in rows])
 
-def pack_by(l, n):
-    rest = l
-    while rest:
-        curr, rest = rest[:999], rest[999:]
-        yield curr
+    def reindex(self):
+        for row in self.db.execute('SELECT distinct sensor FROM climon'):
+            sensor, = row
+            logging.debug('Getting timestamps for %s', sensor)
+            timestamps = [row[0] for row in self.db.execute('\
+                    SELECT time [timestamp]\
+                    FROM climon\
+                    WHERE sensor = ?\
+                    ORDER BY time', (sensor,))]
+
+            for view_range in VIEW_RANGES:
+                self.update_view_stats(sensor, view_range, timestamps)
+        self.commit()
 
 
 if __name__ == '__main__':
--- web.py
+++ web.py
@@ -13,7 +13,7 @@ import database
 app = flask.Flask(__name__)
 conf = None
 
-def db():
+def get_db():
     db = getattr(flask.g, 'db', None)
     if db is None:
         logging.info('Connecting to DB.')
@@ -22,47 +22,48 @@ def db():
 
 @app.teardown_appcontext
 def close_db(error):
-    logging.info('Closing DB.')
+    logging.info('Closing DB: %s', error)
 
 @app.route('/sensor/<sensor_id>')
 def climon(sensor_id):
     temp, hum = sensors.get_by_id(conf, sensor_id)()
     return '%f %f' % (temp, hum)
 
-range_dates = dict(
+RANGE_DATES = dict(
     hour=lambda d: (d - timedelta(hours=1), d),
     day=lambda d: (d - timedelta(days=1), d),
     week=lambda d: (d - timedelta(days=7), d),
     month=lambda d: (d - timedelta(days=30), d),
     year=lambda d: (d - timedelta(days=356), d),
-    all=lambda d: (db().getDateSpan()),
+    all=lambda d: (get_db().get_date_span()),
 )
 
 def utc2local(utc):
     epoch = time.mktime(utc.timetuple())
-    offset = datetime.fromtimestamp (epoch) - datetime.utcfromtimestamp (epoch)
+    offset = datetime.fromtimestamp(epoch) - datetime.utcfromtimestamp(epoch)
     return utc + offset
 
 @app.route('/data/now')
 def gnowdata():
     sensor_data = dict(now=datetime.now().strftime('%Y%m%dT%H%M%S'), sensors={})
     for sensor_id in sensors.iter_ids(conf):
-        d, temp, hum = db().getLatest(sensor_id)
+        _, temp, hum = get_db().get_latest(sensor_id)
         sensor_data['sensors'][sensor_id] = dict(temperature=temp, humidity=hum)
     return json.dumps(sensor_data)
 
-@app.route('/data/<range>/<yyyymmdd>')
-def ganydata(range, yyyymmdd):
-    assert range in range_dates
+@app.route('/data/<view_range>')
+def ganydata(view_range):
+    assert view_range in RANGE_DATES
     day = datetime.utcnow()
-    from_date, to_date = range_dates[range](day)
+    from_date, to_date = RANGE_DATES[view_range](day)
     sensor_data = {}
     labels = []
 
     for sensor_id in sensors.iter_ids(conf):
         sensor_data[sensor_id] = defaultdict(lambda: [])
-        logging.debug("Getting stats for %s" % sensor_id)
-        for d, avg_temp, min_temp, max_temp, avg_hum, min_hum, max_hum in db().get_stats(sensor_id, from_date, to_date, range):
+        logging.debug("Getting stats for %s", sensor_id)
+        for d, avg_temp, min_temp, max_temp, avg_hum, min_hum, max_hum \
+                in get_db().get_stats(sensor_id, from_date, to_date, view_range):
             sensor_data[sensor_id]['temperatures'].append(avg_temp)
             sensor_data[sensor_id]['temperatures_min'].append(min_temp)
             sensor_data[sensor_id]['temperatures_max'].append(max_temp)
@@ -74,7 +75,7 @@ def ganydata(range, yyyymmdd):
         sensor_data[sensor_id]['temperatures'].append(None)
         sensor_data[sensor_id]['humidities'].append(None)
 
-    for d in database.iter_view_times(from_date, to_date, range):
+    for d in database.iter_view_times(from_date, to_date, view_range):
         labels.append(utc2local(d).strftime('%Y%m%dT%H%M%S'))
 
     labels.append(datetime.now().strftime('%Y%m%dT%H%M%S'))
@@ -84,15 +85,15 @@ def ganydata(range, yyyymmdd):
 @app.route('/')
 def index():
     timestamp = datetime.now()
-    sensor_confs=dict((sensor_id, conf['sensor:%s' % sensor_id]) for sensor_id in sensors.iter_ids(conf))
+    sensor_confs = dict((sensor_id, conf['sensor:%s' % sensor_id]) for sensor_id in sensors.iter_ids(conf))
     return render_template('index.html', date=timestamp.strftime('%Y%m%d'), sensor_confs=sensor_confs)
 
 def main(conf_fname, debug=False):
     global conf
 
     logging.basicConfig(filename='climon.log',
-            format='%(asctime)s %(levelname)s WEB %(message)s',
-            level=logging.DEBUG)
+                        format='%(asctime)s %(levelname)s WEB %(message)s',
+                        level=logging.DEBUG)
 
     logging.info('Reading conf')
     conf = read_conf(conf_fname)
@@ -102,19 +103,19 @@ def main(conf_fname, debug=False):
 if __name__ == '__main__':
     import daemon
     import sys
-    
+
     class Daemon(daemon.Daemon):
-    
+
         def run(self):
             main('climon.conf')
-    
+
     daemon = Daemon(pidfile='web.pid')
-    
-    if 'start' == sys.argv[1]: 
+
+    if 'start' == sys.argv[1]:
         daemon.start()
-    elif 'stop' == sys.argv[1]: 
+    elif 'stop' == sys.argv[1]:
         daemon.stop()
-    elif 'restart' == sys.argv[1]: 
+    elif 'restart' == sys.argv[1]:
         daemon.restart()
     elif 'debug' == sys.argv[1]:
         main('climon.conf', debug=True)
